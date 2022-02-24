f1bd0f91836f21f2d02ed54ea2d1b9a2f17e5880
syko <aa.andrin@gmail.com>
2022-02-24 14:40:37 +0200

    Add The Interactive Thing

---
+++ components/the-interactive-thing.tsx
@@ -0,0 +1,276 @@
+import React, { RefObject } from 'react'
+import * as THREE from 'three'
+import { WebGLRenderer, OrthographicCamera, Scene, BufferGeometry, Points, PointsMaterial, Color, BufferAttribute, Vector2, Vector3, Texture } from 'three'
+import Stats from 'three/examples/jsm/libs/stats.module'
+
+const CONTENTFUL_PIXEL_THRESHOLD = 6;
+
+export type PixelData = Array<Float32Array>;
+type ImageDataProps = {
+  imageData: ImageData;
+}
+
+type PointerCollection = {
+  mouse: Vector2;
+  [index: number]: Vector2;
+}
+
+class TheInteractiveThing extends React.Component<ImageDataProps> {
+  container: RefObject<HTMLDivElement>;
+  windowWidth: number = 0;
+  windowHeight: number = 0;
+  pointers: PointerCollection = {
+    'mouse': new THREE.Vector2(0, 0)
+  }
+  lastPointers: PointerCollection = {
+    'mouse': new THREE.Vector2(0, 0)
+  }
+  camera!: OrthographicCamera;
+  scene!: Scene;
+  renderer!: WebGLRenderer;
+  stats!: Stats;
+  particles!: Points;
+  originalPositions!: BufferAttribute;
+  targetPositions!: BufferAttribute;
+  velocities!: BufferAttribute;
+  influenceRanges: Float32Array = new Float32Array(20);
+
+  constructor(props: {imageData: ImageData}) {
+    super(props);
+    this.container = React.createRef();
+    this.populateInfluenceRanges();
+  }
+
+  componentDidMount() {
+    this.setupThree();
+
+    this.createParticles();
+    // this.createBackground();
+    this.updateSize();
+
+    this.container.current!.appendChild(this.renderer.domElement);
+    this.container.current!.appendChild(this.stats.dom);
+
+    this.addEventListeners();
+
+    this.tick();
+  }
+
+  componentWillUnmount() {
+    this.teardownThree();
+    this.removeEventListeners();
+  }
+
+  populateInfluenceRanges() {
+    for (let i = 0; i < this.influenceRanges.length; i++) {
+      this.influenceRanges[i] = 5000 + Math.random() * 5000;
+    }
+  }
+
+  getContentfulPixels(imageData: ImageData): PixelData {
+    const { width, height, data } = imageData;
+    const pixels: PixelData = [];
+    let x, y;
+    for (let i = 0; i < data.length; i += 4) {
+      if (data[i] + data[i + 1] + data[i + 2] < CONTENTFUL_PIXEL_THRESHOLD) continue;
+      x = (i / 4) % width;
+      y = height - Math.floor((i / 4 - x) / width);
+      pixels.push(new Float32Array([x, y, data[i], data[i + 1], data[i + 2]]));
+    }
+    return pixels;
+  }
+
+  setupThree() {
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.OrthographicCamera(0, window.innerWidth, window.innerHeight, 0, 0, 100);
+    this.camera.position.z = 100;
+
+    this.stats = new (Stats as any)();
+
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+  }
+
+  teardownThree() {
+    this.renderer.dispose();
+  }
+
+  createBackground() {
+    const scene = this.scene;
+    new THREE.TextureLoader().load('/lawn.png', (texture) => {
+      const material = new THREE.SpriteMaterial({ map: texture, color: 0xffffff });
+      const sprite = new THREE.Sprite(material);
+      sprite.scale.set(texture.image.width, texture.image.height, 1);
+      sprite.position.add(this.particles.position);
+      sprite.position.setZ(1);
+      scene.add(sprite);
+    });
+  }
+
+  populatePositions(pixelData: PixelData): Float32Array {
+    const { width, height } = this.props.imageData;
+    const positions = new Float32Array(pixelData.length * 3);
+    for (let i = 0; i < pixelData.length; i++) {
+      let j = i * 3;
+      positions[j] = pixelData[i][0] - width / 2;
+      positions[j + 1] = pixelData[i][1] - height / 2;
+      positions[j + 2] = 0;
+    }
+    this.originalPositions = new THREE.BufferAttribute(positions.slice(), 3);
+    this.targetPositions = new THREE.BufferAttribute(positions.slice(), 3);
+    this.velocities = new THREE.BufferAttribute(new Float32Array(positions.length), 3);
+    return positions;
+  }
+
+  populateColors(pixelData: PixelData): Float32Array {
+    const colors = new Float32Array(pixelData.length * 3);
+    for (let i = 0; i < pixelData.length; i++) {
+      let j = i * 3;
+      colors[j] = pixelData[i][2] / 255;
+      colors[j + 1] = pixelData[i][3] / 255;
+      colors[j + 2] = pixelData[i][4] / 255;
+    }
+    return colors;
+  }
+
+  createParticles() {
+    const pixelData = this.getContentfulPixels(this.props.imageData);
+
+    const positions = this.populatePositions(pixelData);
+    const colors = this.populateColors(pixelData);
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+    this.particles = new THREE.Points(geometry, new THREE.PointsMaterial({vertexColors: true, size: 1}));
+    this.scene.add(this.particles);
+  }
+
+  addEventListeners() {
+    this.container.current!.addEventListener('pointermove', this.onMouseMove);
+    this.container.current!.addEventListener('touchstart', this.onTouchMove);
+    this.container.current!.addEventListener('touchmove', this.onTouchMove);
+    this.container.current!.addEventListener('touchend', this.onTouchEnd);
+    this.container.current!.addEventListener('touchcancel', this.onTouchEnd);
+    window.addEventListener('resize', this.onWindowResize);
+  }
+
+  removeEventListeners() {
+    this.container.current!.removeEventListener('pointermove', this.onMouseMove);
+    this.container.current!.removeEventListener('touchstart', this.onTouchMove);
+    this.container.current!.removeEventListener('touchmove', this.onTouchMove);
+    this.container.current!.removeEventListener('touchend', this.onTouchEnd);
+    this.container.current!.removeEventListener('touchcancel', this.onTouchEnd);
+    window.removeEventListener('resize', this.onWindowResize);
+  }
+
+  onWindowResize = () => {
+    this.updateSize();
+  }
+
+  onMouseMove = (e: PointerEvent) => {
+    if (!e.isPrimary) return;
+    this.lastPointers.mouse.set(this.pointers.mouse.x, this.pointers.mouse.y);
+    this.pointers.mouse.set(e.clientX, this.windowHeight - e.clientY);
+  }
+
+  ensureTouchExists(id: number, x: number, y: number) {
+    if (this.pointers[id]) return;
+    this.pointers[id] = new Vector2(x, y);
+    this.lastPointers[id] = new Vector2(x, y);
+  }
+
+  onTouchStart = (e: TouchEvent) => {
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      const touch = e.changedTouches[i];
+      const id = touch.identifier;
+      this.pointers[id] = new Vector2(touch.clientX, touch.clientY);
+      this.lastPointers[id] = this.pointers[id];
+    }
+  }
+
+  onTouchMove = (e: TouchEvent) => {
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      const touch = e.changedTouches[i];
+      const id = touch.identifier;
+      this.ensureTouchExists(id, touch.clientX, this.windowHeight - touch.clientY);
+      this.lastPointers[id].set(this.pointers[id].x, this.pointers[id].y);
+      this.pointers[id].set(touch.clientX, this.windowHeight - touch.clientY);
+    }
+  }
+
+  onTouchEnd = (e: TouchEvent) => {
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      const id = e.changedTouches[i].identifier;
+      delete this.pointers[id];
+      delete this.lastPointers[id];
+    }
+  }
+
+  updateSize() {
+    const { innerWidth: w, innerHeight: h } = window;
+    this.windowWidth = w;
+    this.windowHeight = h;
+    this.camera.top = h;
+    this.camera.right = w;
+    this.renderer.setSize(w, h);
+    this.particles.position.set(w / 2, h / 2, 2);
+    this.camera.updateProjectionMatrix();
+  }
+
+  updateParticles() {
+    const positions = this.particles.geometry.attributes.position;
+    const parentOffset: Vector2 = new THREE.Vector2(this.particles.position.x, this.particles.position.y);
+    for (let k in this.pointers) {
+      let x, y, tx, ty, vx, vy;
+      const mousePosAdjusted = this.pointers[k].clone().sub(parentOffset);
+      const pos: Vector2 = new THREE.Vector2(0, 0);
+      let particleDistance: Vector2 = new THREE.Vector2(0, 0);
+      let moveAmount: Vector2 = new THREE.Vector2(0, 0);
+      // Update targePositions
+      for (let i = 0; i < positions.count; i++) {
+        pos.set(positions.getX(i), positions.getY(i));
+        particleDistance = mousePosAdjusted.clone().sub(pos);
+        if (particleDistance.lengthSq() < this.influenceRanges[i % this.influenceRanges.length]) {
+          moveAmount = particleDistance.clone().normalize().multiplyScalar(0 + Math.random() * 165 - particleDistance.length());
+          this.targetPositions.setXY(i, pos.x - moveAmount.x, pos.y - moveAmount.y);
+        } else this.targetPositions.setXY(i, this.originalPositions.getX(i), this.originalPositions.getY(i));
+      }
+      // Update velocities based on target positions and current velocities and update positions
+      for (let i = 0; i < positions.count; i++) {
+        x = positions.getX(i);
+        y = positions.getY(i);
+        vx = this.velocities.getX(i);
+        vy = this.velocities.getY(i);
+        tx = this.targetPositions.getX(i);
+        ty = this.targetPositions.getY(i);
+        vx = (vx + (tx - x) * 0.02) * 0.91;
+        vy = (vy + (ty - y) * 0.02) * 0.91;
+        this.velocities.setXY(i, vx, vy);
+        positions.setXY(i, x + vx, y + vy);
+      }
+    }
+  }
+
+  tick = () => {
+    requestAnimationFrame(this.tick);
+    this.renderThree();
+    if(this.stats) this.stats.update();
+  }
+
+  renderThree() {
+    this.updateParticles();
+    this.particles.geometry.attributes.position.needsUpdate = true;
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  render() {
+    return (
+      <div ref={this.container} />
+    )
+  }
+}
+
+export default TheInteractiveThing;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,46 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import React, { RefObject } from 'react'
 import styles from '../styles/Index.module.css'
+import TheInteractiveThing from '../components/the-interactive-thing';
 
-const Index: NextPage = () => {
-  return (
-    <h1 className={styles.title}>Get Off My Lawn Entertainment</h1>
-  )
+const IMAGE_URL = '/lawn.png';
+
+class Index extends React.Component<{}, { imageLoaded: boolean }> {
+  canvas: RefObject<HTMLCanvasElement>;
+  imageData!: ImageData;
+
+  constructor(props: {}) {
+    super(props);
+    this.canvas = React.createRef();
+    this.state = { imageLoaded: false };
+  }
+
+  componentDidMount() {
+    const img = new Image();
+    const canvas = this.canvas.current!;
+    const ctx = canvas.getContext("2d")!;
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+      this.imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      this.setState({ imageLoaded: true });
+    }
+    img.src = IMAGE_URL;
+  }
+
+  render() {
+    return (
+      <main>
+        <Head>
+          <title>Get Off My Lawn Entertainment - Next-gen AAA Game Studio</title>
+          <meta name="description" content="Next-gen AAA Game Studio" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+        </Head>
+        <h1 className={styles.title}>Get Off My Lawn Entertainment</h1>
+        <canvas ref={this.canvas} width="948" height="300" className={styles.inputCanvas}></canvas>
+        {this.state.imageLoaded && <TheInteractiveThing imageData={this.imageData} />}
+      </main>
+    )
+  }
 }
 
 export default Index
